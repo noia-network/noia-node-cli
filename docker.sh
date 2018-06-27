6ecfb5e4830b1dc692897839ad5044bbbc8eaa15
dagelf <coenraad@wish.org.za>
2018-06-27 20:22:11 +0200

    fixed port

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 docker build . --tag noia/alpine
-docker run --rm -p :6767:6767 -e WALLET_ADDRESS=0x4adc7773bceedf7b4c687f9bfdd1598378b7d5e1 noia/alpine
+docker run --rm -p :7676:7676 -e WALLET_ADDRESS=0x4adc7773bceedf7b4c687f9bfdd1598378b7d5e1 noia/alpine
